3837735a6eddc6b1886f1da35fcfe7d19bdfa5a5
wilsondesouza <wilson.de.souza.junior@gmail.com>
2023-08-21 20:04:25 -0300

    Atualizando simples estilos

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Projeto Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "Página Inicial"),
     );
   }
 }
@@ -106,19 +106,23 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você apertou o botão:',
+              style: TextStyle(color: Colors.deepPurple, fontSize: 22)
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headlineLarge,
             ),
+            const Text(
+              'vezes ao total',
+              style: TextStyle( color: Colors.blueAccent, fontSize: 22))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.arrow_forward_rounded),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
